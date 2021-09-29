5f3b3c12ecbc2bde53e099c6a73fc8728655819d
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2021-09-29 21:30:40 +0200

    new font size, instant is no longer default use -n

--- config.h
+++ config.h
@@ -1,11 +1,13 @@
 /* See LICENSE file for copyright and license details. */
 /* Default settings; can be overriden by command line. */
 
-static int instant = 1;
+static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"Liberation Mono for Powerline:size=12"
+	"LiterationMono Nerd Font:pixelsize=14:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=12:antialias=true:autohint=true",
+	"Latin Modern Math:pixelsize=14:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
