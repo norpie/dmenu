19c88b0f0783e549b8ac456c1375fdbd75e6e34f
Konsta Kuosmanen <konstamskuosmanen@gmail.com>
2021-10-19 14:35:59 +0200

    nord theme compatibility

--- config.h
+++ config.h
@@ -5,9 +5,9 @@ static int instant = 0;
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
 /* -fn option overrides fonts[0]; default X11 font or font set */
 static const char *fonts[] = {
-	"LiterationMono Nerd Font:pixelsize=14:antialias=true:autohint=true",
-	"JoyPixels:pixelsize=12:antialias=true:autohint=true",
-	"Latin Modern Math:pixelsize=14:antialias=true:autohint=true",
+	"LiterationMono Nerd Font:pixelsize=20:antialias=true:autohint=true",
+	"JoyPixels:pixelsize=18:antialias=true:autohint=true",
+	"Latin Modern Math:pixelsize=18:antialias=true:autohint=true",
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
